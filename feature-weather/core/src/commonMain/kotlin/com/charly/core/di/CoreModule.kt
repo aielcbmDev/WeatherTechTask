6789ee3dfff6ba14ceaf7398d4c8eab063b1d03f
Charly Baquero <aielcbm@gmail.com>
2025-11-19 14:08:01 +0100

    refactor: Remove redundant domain module dependency

--- feature-weather/core/src/commonMain/kotlin/com/charly/core/di/CoreModule.kt
+++ feature-weather/core/src/commonMain/kotlin/com/charly/core/di/CoreModule.kt
@@ -4,7 +4,6 @@ import com.charly.core.cache.TimerCache
 import com.charly.core.repositories.DailyWeatherForecastRepositoryImpl
 import com.charly.database.di.databaseModule
 import com.charly.datastore.di.datastoreModule
-import com.charly.domain.di.domainModule
 import com.charly.domain.repositories.DailyWeatherForecastRepository
 import com.charly.networking.di.networkingModule
 import org.koin.dsl.module
@@ -16,7 +15,6 @@ val coreModule = module {
     includes(databaseModule)
     includes(datastoreModule)
     includes(networkingModule)
-    includes(domainModule)
 
     single<Clock> { Clock.System }
     single<TimerCache> {
