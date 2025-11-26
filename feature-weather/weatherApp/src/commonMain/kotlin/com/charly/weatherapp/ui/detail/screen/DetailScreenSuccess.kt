14a6efb94d6cf63011664acdc43a35977a60e8b6
Charly Baquero <aielcbm@gmail.com>
2025-11-26 11:43:50 +0100

    feat: Make detail screen vertically scrollable
    Adds a `verticalScroll` modifier to the `Column` in `DetailScreenSuccess`.
    
    This change ensures that the content on the detail screen can scroll vertically, preventing UI elements from being cut off on smaller devices.

--- feature-weather/weatherApp/src/commonMain/kotlin/com/charly/weatherapp/ui/detail/screen/DetailScreenSuccess.kt
+++ feature-weather/weatherApp/src/commonMain/kotlin/com/charly/weatherapp/ui/detail/screen/DetailScreenSuccess.kt
@@ -3,6 +3,8 @@ package com.charly.weatherapp.ui.detail.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -52,7 +54,9 @@ fun DetailScreenSuccess(
     ) { padding ->
         Column(
             horizontalAlignment = Alignment.Start,
-            modifier = Modifier.padding(padding)
+            modifier = Modifier
+                .padding(padding)
+                .verticalScroll(rememberScrollState())
         ) {
             DisplayDataHorizontally(
                 stringResource(Res.string.detail_screen_sunrise_title),
