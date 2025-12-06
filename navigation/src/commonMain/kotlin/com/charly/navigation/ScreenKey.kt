3d8f44e7e6e3c623f71e60f68be39bad7d79b435
Charly Baquero <aielcbm@gmail.com>
2025-12-06 11:56:37 +0100

    feat: Enable Kotlin Serialization for navigation keys
    Adds the Kotlin Serialization plugin to the `navigation` module and marks the `MainScreenKey` and `DetailScreenKey` `NavKey` implementations with the `@Serializable` annotation.
    
    This enables the navigation keys to be serialized, which is a necessary step for state restoration and passing them as arguments.

--- navigation/src/commonMain/kotlin/com/charly/navigation/ScreenKey.kt
+++ navigation/src/commonMain/kotlin/com/charly/navigation/ScreenKey.kt
@@ -1,6 +1,10 @@
 package com.charly.navigation
 
 import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
 
+@Serializable
 data object MainScreenKey : NavKey
+
+@Serializable
 data class DetailScreenKey(val id: Long) : NavKey
