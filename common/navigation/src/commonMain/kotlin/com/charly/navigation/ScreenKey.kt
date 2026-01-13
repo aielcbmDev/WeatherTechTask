6997780fb2d096af11537085b8e864f53326e11d
Charly Baquero <aielcbm@gmail.com>
2026-01-13 17:40:22 +0100

    feat: Revert previous change and convert MainScreenKey to a data object
    Converts `MainScreenKey` from a regular `object` to a `data object`. This change ensures that the singleton instance correctly implements `toString()`, `equals()`, and `hashCode()`, which is beneficial for serialization and consistency within the `ScreenKey` sealed interface.

--- common/navigation/src/commonMain/kotlin/com/charly/navigation/ScreenKey.kt
+++ common/navigation/src/commonMain/kotlin/com/charly/navigation/ScreenKey.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 sealed interface ScreenKey : NavKey {
 
     @Serializable
-    object MainScreenKey : ScreenKey
+    data object MainScreenKey : ScreenKey
 
     @Serializable
     data class DetailScreenKey(val id: Long) : ScreenKey
