a7a4044d91d9a0f0a4384da927e4184cf806b045
Charly Baquero <aielcbm@gmail.com>
2026-02-09 14:13:54 +0000

    refactor: Remove unused `@Preview` annotation
    Removes an unused `@Preview` annotation and its corresponding import from the `App` composable. This preview was not functional on a Composable that accepts parameters.

--- composeApp/src/commonMain/kotlin/com/something/volkswagentechtask/App.kt
+++ composeApp/src/commonMain/kotlin/com/something/volkswagentechtask/App.kt
@@ -3,7 +3,6 @@ package com.something.volkswagentechtask
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.charly.uitheme.DarkColors
 import com.charly.uitheme.LightColors
 import com.charly.uitheme.typography
@@ -14,7 +13,6 @@ import org.koin.compose.KoinApplication
 import org.koin.dsl.KoinAppDeclaration
 
 @Composable
-@Preview
 fun App(
     koinAppDeclaration: KoinAppDeclaration? = null,
     darkTheme: Boolean = isSystemInDarkTheme(),
